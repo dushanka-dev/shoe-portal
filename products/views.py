f20c85f5467e2684bf80dac87412bedf867cd0bc
Dushanka Perera <dushankadev@gmail.com>
2022-04-19 16:30:49 +0000

    Create listview for product model

--- products/views.py
+++ products/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Product
 
 # Create your views here.
 
-def products(request):
-    """View to return index page """
+# def products(request):
+#     """View to return index page """
 
-    return render(request, 'products/products.html')
+#     return render(request, 'products/products.html')
+
+
+class AllProducts(ListView):
+    """List all product to page"""
+
+    model = Product
+    template_name = 'products/products.html'
+    context_object_name = 'products'
