aa2b760ad6a3b73659e1ec50b3912881a69f805c
Dushanka Perera <dushankadev@gmail.com>
2022-04-19 15:14:58 +0000

    Create view to render product page

--- products/views.py
+++ products/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def products(request):
+    """View to return index page """
+
+    return render(request, 'products/products.html')
