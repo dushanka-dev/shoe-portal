704dda7a40e335a895182c2f7dee50e315968c82
Dushanka Perera <dushankadev@gmail.com>
2022-04-23 16:36:41 +0000

    Connect category page to view

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 # from . import views
-from .views import AllProducts, ProductDetail
+from .views import AllProducts, Categories, ProductDetail
 
 urlpatterns = [
     path('allproducts/', AllProducts.as_view(), name='all-products'),
+    path('categories/', Categories.as_view(), name='categories'),
     path('product/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
     # path('product/', views.products, name='products'),
 ]
