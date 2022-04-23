eb3f37b9c381341d052fbd7fd44a19fa0e3bcf80
Dushanka Perera <dushankadev@gmail.com>
2022-04-23 12:25:44 +0000

    Remove unused code

--- products/views.py
+++ products/views.py
@@ -4,11 +4,6 @@ from .models import Product
 
 # Create your views here.
 
-# def products(request):
-#     """View to return index page """
-
-#     return render(request, 'products/product_detail.html')
-
 
 class AllProducts(ListView):
     """List all product to page"""
@@ -24,10 +19,4 @@ class ProductDetail(DetailView):
     model = Product
     template_name = 'products/product_detail.html'
     context_object_name = 'product_detail'
-
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-
-
-
 
