b3068c22abef333dbfb567caf148d3468f18a7ab
Dushanka Perera <dushankadev@gmail.com>
2022-05-02 12:51:51 +0000

    Remove allproducts cbv url path

--- products/urls.py
+++ products/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import AllProducts, Categories, ProductDetail
+from .views import Categories, ProductDetail
 
 urlpatterns = [
-    path('allproducts/', AllProducts.as_view(), name='all-products'),
+    # path('allproducts/', AllProducts.as_view(), name='all-products'),
     path('categories/', Categories.as_view(), name='categories'),
     # path('productcategory/', ProductsCategory.as_view(), name='product-category'),
     path('product-detail/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
