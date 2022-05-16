d4d0e61983ceeb8290f1ce3879cb3d1faeed8a1e
Dushanka Perera <dushankadev@gmail.com>
2022-05-16 13:56:02 +0000

    Add cart context and delivery logic

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='cart')
+    path('', views.cart_view, name='cart_view')
 ]
--- cart/views.py
+++ cart/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def cart_view(request):
     """View to return index page """
 
     return render(request, 'cart/cart.html')
