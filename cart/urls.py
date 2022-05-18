ed2d0a4a7955f22a595178eac52e19199ebb9c42
Dushanka Perera <dushankadev@gmail.com>
2022-05-18 16:01:00 +0000

    Add add to cart logic and add form

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import AddToCart
 
 urlpatterns = [
-    path('', views.cart_view, name='cart_view')
+    path('', views.cart_view, name='cart_view'),
+    path('add/<item_id>/', AddToCart.as_view(), name='add_to_cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
 
 # Create your views here.
+
 
 def cart_view(request):
     """View to return index page """
 
     return render(request, 'cart/cart.html')
+
+
+class AddToCart(View):
+    """ Add products to shopping cart """
+
+    def post(self, request, item_id):
+        """ Add a quantity of the product to the cart """
+
+        quantity = int(request.POST.get('quantity'))
+        redirect_url = request.POST.get('redirect_url')
+        cart = request.session.get('cart', {})
+
+        if item_id in list(cart.keys()):
+            cart[item_id] += quantity
+        else:
+            cart[item_id] = quantity
+
+        request.session['cart'] = cart
+        print(request.session['cart'])
+        return redirect(redirect_url)
