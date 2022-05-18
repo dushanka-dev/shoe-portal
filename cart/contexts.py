adf60e55bc2ec92b39047aa6e77879db0b68868a
Dushanka Perera <dushankadev@gmail.com>
2022-05-18 17:31:42 +0000

    Create logic to render items details to all templates

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,8 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from products.models import Product
+
 
 def cart_content(request):
     """Cart items context to allow use of the cart item functionality throughout the site"""
@@ -7,6 +10,17 @@ def cart_content(request):
     cart_items = []
     total = 0
     product_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        product = get_object_or_404(Product, pk=item_id)
+        total += quantity * product.price
+        product_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -14,7 +28,7 @@ def cart_content(request):
     else:
         delivery = 0
         free_delivery_delta = 0
-    
+
     grand_total = delivery + total
 
     context = {
