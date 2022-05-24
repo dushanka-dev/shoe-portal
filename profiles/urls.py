4dc7c4c6ec6e598a1db0029dfb212bba8d095ec6
Dushanka Perera <dushankadev@gmail.com>
2022-05-24 16:19:49 +0000

    Update allauth page structures

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-# from .views import Profiles
+from .views import Profiles
 
 urlpatterns = [
     path('usersprofile/', views.profiles, name='profile'),
-    # path('usersprofile/<int:pk>/', Profiles.as_view(), name='profile'),
+    # path('usersprofile/', Profiles.as_view(), name='profile'),
 ]
