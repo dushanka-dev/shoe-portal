9e03dcde32449ea22ba9d64071a5f03aeab61e50
Dushanka Perera <dushankadev@gmail.com>
2022-05-26 16:21:04 +0000

    Connect edit cart view to urls

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import AddToCart
+from .views import AddToCart, EditCart
 
 urlpatterns = [
     path('', views.cart_view, name='cart_view'),
     path('add/<item_id>/', AddToCart.as_view(), name='add_to_cart'),
+    path('edit-cart/<item_id>/', EditCart.as_view(), name='edit_cart'),
 ]
