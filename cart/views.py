6429946168352cf876d03fd2d251aa0f1c126251
Dushanka Perera <dushankadev@gmail.com>
2022-05-26 15:26:22 +0000

    Create logic to update cart items

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.views.generic import View
 
 # Create your views here.
@@ -40,3 +40,33 @@ class AddToCart(View):
         request.session['cart'] = cart
         print(request.session['cart'])
         return redirect(redirect_url)
+
+
+class EditCart(View):
+    """ Add products to shopping cart """
+
+    def post(self, request, item_id):
+        """ Add a quantity of the product to the cart """
+
+        quantity = int(request.POST.get('quantity'))
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        cart = request.session.get('cart', {})
+
+        if size:
+            if quantity > 0:
+                cart[item_id]['items_by_size'][size] = quantity
+            else:
+                del cart[item_id]['items_by_size'][size]
+                if not cart[item_id]['items_by_size']:
+                    cart.pop(item_id)
+        else:
+            if quantity > 0:
+                cart[item_id] = quantity
+            else:
+                cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return redirect(reverse('cart_view'))
+
