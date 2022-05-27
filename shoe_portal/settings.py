c277e6376d8c16fc7f1a2c79bff30dbd53e48e05
Dushanka Perera <dushankadev@gmail.com>
2022-05-27 19:54:20 +0000

    Add crispy forms tags site wide availbility

--- shoe_portal/settings.py
+++ shoe_portal/settings.py
@@ -82,6 +82,10 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 'cart.contexts.cart_content'
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
