77d9d36c2d1a4b0ff2173a539ffc7ae32633ec26
Dushanka Perera <dushankadev@gmail.com>
2022-05-28 12:24:54 +0000

    Connect stripe with pk and sk. Add client int to view

--- checkout/views.py
+++ checkout/views.py
@@ -1,24 +1,45 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
 from django.views.generic import View
+from django.conf import settings
 
+from cart.contexts import cart_content
 from .forms import OrderForm
+
+import stripe
+
 
 class CheckoutForm(View):
     """Checkout form"""
 
     def get(self, request):
+
+        stripe_public_key = settings.STRIPE_PUBLIC_KEY
+        stripe_secret_key = settings.STRIPE_SECRET_KEY
+
         cart = request.session.get('cart', {})
         if not cart:
             messages.error(request, "There's nothing in your cart at the moment")
             return redirect(reverse('products'))
 
+        current_cart = cart_content(request)
+        total = current_cart['grand_total']
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        print(intent)
+
+
         order_form = OrderForm()
         template = 'checkout/checkout.html'
         context = {
             'order_form': order_form,
-            'stripe_public_key': 'pk_test_51L4NLsAhQRhfFaYIIkXALsCPRIG1bpYROz2IxgxE9KZDHEMoo2otUuWXFwdQHrYI47FCm5WVOo9NkZVqMky1t5uz009Y90jvcS',
-            'client_secret': 'test client secret',
+            'stripe_public_key': stripe_public_key,
+            'client_secret': intent.client_secret,
         }
 
         return render(request, template, context)
