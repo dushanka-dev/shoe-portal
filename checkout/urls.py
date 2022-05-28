5c3c8ccbccfcf893c4815bc2b27a3a8f4c111143
Dushanka Perera <dushankadev@gmail.com>
2022-05-28 18:15:39 +0000

    Add class methods to webhook handler and webhooks

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from .views import CheckoutForm, CheckoutSuccess
+from .webhooks import webhook
 
 urlpatterns = [
     path('', CheckoutForm.as_view(), name='checkout'),
     path('checkout-success/<order_number>', CheckoutSuccess.as_view(), name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
