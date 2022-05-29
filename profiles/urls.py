be7ed16085ffdc103bbf9c76248d795e52b6aa41
Dushanka Perera <dushankadev@gmail.com>
2022-05-29 20:17:13 +0000

    Connect profiles and userorder views

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import Profiles
+from .views import Profiles, UserOrdersView
 
 urlpatterns = [
     # path('usersprofile/', views.profiles, name='profile'),
     path('profile/', Profiles.as_view(), name='profile'),
+    path('order-history/<order_number>', UserOrdersView.as_view(), name='order-history'),
 ]
